9fde6237a4ea48c8fba9dd920fafdc3968af4b2a
flawlesscode254 <denzme414@gmail.com>
2020-12-18 13:25:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function Change(){
+    let free = document.getElementById("col").value;
+    let der = document.getElementById("one");
+    der.style.backgroundColor = free;
+};
+
+function Figure(){
+    let two = document.getElementById("in").value;
+    let three = document.getElementById("two");
+    three.innerHTML = two;
+}
